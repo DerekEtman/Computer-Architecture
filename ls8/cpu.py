46056a2860e6cc16083495a24de8d8712dc0d4a5
Derek Etman <dereketman@gmail.com>
2020-02-26 23:22:17 -0800

    + MUL

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,9 +2,12 @@
 
 import sys
 
-LDI = 0b10000010
-PRN = 0b01000111
-HLT = 0b00000001
+
+# **No longer need these since I label them in the switchbranch**
+# LDI = 0b10000010
+# PRN = 0b01000111
+# HLT = 0b00000001
+# MUL = 10100010
 
 
 
@@ -27,9 +30,10 @@ class CPU:
         self.mdr = 0
         # Branch table holding functions and the IR Opcode value
         self.branchtable = {}
-        self.branchtable[0b10000010] = {'instruction':'LDI', 'retrieve': self.LDI_handler}
-        self.branchtable[0b01000111] = {'instruction':'PRN', 'retrieve': self.PRN_handler}
-        self.branchtable[0b00000001] = {'instruction':'HLT', 'retrieve': self.HLT_handler}
+        self.branchtable[0b10000010] = {'instruction_name':'LDI', 'retrieve': self.LDI_handler}
+        self.branchtable[0b01000111] = {'instruction_name':'PRN', 'retrieve': self.PRN_handler}
+        self.branchtable[0b00000001] = {'instruction_name':'HLT', 'retrieve': self.HLT_handler}
+        self.branchtable[0b10100010] = {'instruction_name':'MUL', 'retrieve': self.alu}
 
     
 
@@ -90,6 +94,7 @@ class CPU:
         #     address += 1
 
     def LDI_handler(self,a):
+        print(f"\n--Running LDI--")
         # print(f"Op_a: {operand_a}, Op b: {operand_b}")
         address = self.ram_read(self.pc + 1)
         value = self.ram_read(self.pc + 2)
@@ -98,10 +103,15 @@ class CPU:
         self.pc += 3        
 
     def PRN_handler(self,a):
-        pass
+        print(f"\n--Printing--")
+        address = self.ram_read(self.pc + 1)
+
+        self.reg_read(address)
+        self.pc += 2
 
     def HLT_handler(self,a):
-        pass
+        print(f"\n--Finished--")
+        sys.exit(2)
 
 
     def ram_read(self, mar):
@@ -112,19 +122,19 @@ class CPU:
         # mar holds the Address
         # mdr holds the Value
 
-        self.mdr = self.ram[mar]
-        return self.mdr
+        return self.ram[mar]
 
 
     def ram_write(self,mdr, mar):
-       ''' `raw_write()` should accept a value to write, and the address to write it to.
+       ''' 
+       `raw_write()` should accept a value to write, and the address to write it to.
        '''
        self.ram[mar] = mdr
        return self.ram[mar]
 
     def reg_read(self, mar):
-        self.mdr = self.reg[mar]
-        print(f"reg_read: {self.mdr}")
+        return self.reg[mar]
+
 
     def reg_write(self, mar, mdr):
         self.reg[mar] = mdr
@@ -133,16 +143,27 @@ class CPU:
 
 
 
-    def alu(self, op, reg_a, reg_b):
-        """ALU operations."""
+    def alu(self, op):
+        """ALU operations."""   
+        
+        reg_a = self.ram_read(self.pc + 1)
+        reg_b = self.ram_read(self.pc + 2)
+        # print(f"self.reg_read(reg_a): {self.reg_read(self.pc + 1)} * self.reg_read(reg_b): ")
 
         if op == "ADD":
+            print(f"--ADD--")
             self.reg[reg_a] += self.reg[reg_b]
         #elif op == "SUB": etc
-        if op == "MUL":
-            self.reg[reg_a] *= self.reg[reg_b]
+
+        # Multiply the values in two registers together and store the result in registerA.
+        elif op == "MUL":
+            print(f"--Multiplying--")
+            value = self.reg_read(reg_a) * self.reg_read(reg_b)
+            self.reg_write(reg_a,value)
         else:
             raise Exception("Unsupported ALU operation")
+
+        self.pc += 3
 
     def HLT(self):
         sys.exit(0)
@@ -172,16 +193,20 @@ class CPU:
 
         while True:
 
+            # Grabbing the instructions from RAM
             IR = self.ram[self.pc]
-
-            operand_a = self.ram_read(self.pc + 1)
-            operand_b = self.ram_read(self.pc + 2)
             # print(f"PC at start: {self.pc}, IR: {IR}, operand_a: {operand_a}, operand_b : {operand_b}")
             # print(f"{self.ram}")
+
+            # Grab instructions from branchtable 
             get = self.branchtable[IR]
 
-
-            get['retrieve'](get["instruction"])
+            # Accessing correct instruction from the branchtable
+            # Calling the correct handler from Branchtable
+            # Same as calling: 
+                # if IR == LDI:
+                    # self.LDI_handler()
+            get['retrieve'](get["instruction_name"])
 
             # if IR == LDI:
             #     self.LDI_handler()
